53e51c7858b7262aba57e55cd7d9f6b459409ec8
grepfox <grepfox@tutamail.com>
2025-05-21 22:03:51 +0530

    scripts: Add build script
    * for LineageOS
    
    Change-Id: I587a541ca9c582853604a5d66fb32a4c9f66d590

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+clear
+
+# Check for root
+if [ "$(id -u)" -eq "0" ]; then
+    echo "Do not run this script as root. Please run as a regular user."
+    exit 1
+fi
+
+if [ "$SYNC_WITH_LINEAGE" == "true" ]; then
+    echo "Syncing with LineageOS source"
+
+    # Repo initialization
+    repo init -u https://github.com/grepfox/android.git -b lineage-22.2 --git-lfs --depth=1
+    if [ $? -ne 0 ]; then
+        echo "Repo init failed, but continuing with the build process"
+    fi
+
+    # Sync the repo
+    repo sync -j16 --force-sync
+    if [ $? -ne 0 ]; then
+        echo "Repo sync failed, but continuing with the build process"
+    fi
+else
+    echo "SYNC_WITH_LINEAGE is not set to true, skipping repo init and sync. Continuing with the build process"
+fi
+
+# Define the device
+DEVICE="vayu"
+
+# Set up the directories
+WORK_DIR="$HOME/lineage"
+mkdir -p "$WORK_DIR"
+cd "$WORK_DIR"
+
+#  Set up ccache
+echo "Setting up ccache."
+export USE_CCACHE=1
+export CCACHE_DIR="$WORK_DIR/.ccache"
+export CCACHE_MAXSIZE=50G 
+ccache -M 50G
+
+# Set up the environment for the build
+echo "Setting up the environment"
+source build/envsetup.sh
+
+# Choose the build target for LineageOS
+echo "Building the LineageOS ROM using brunch"
+brunch "$DEVICE"
